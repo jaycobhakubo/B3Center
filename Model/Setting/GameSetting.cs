7ec693ad182838cc0cfa688abd663edd1ca5bd82
kkkkk <karlocamacho@gmail.com>
2017-01-22 03:14:48 -0800

    GameSetting.cs done

--- Model/Setting/GameSetting.cs
+++ Model/Setting/GameSetting.cs
@@ -7,13 +7,22 @@ namespace GameTech.Elite.Client.Modules.B3Center.Model.Setting
 {
     public class GameSettings
     {
-        public string TimeToCollect { get; set; }
-        public bool ScreenCursor { get; set; }
-        public bool CalibrateTouch { get; set; }
-        public bool AnnounceCall { get; set; }
-        public bool PressToCollect { get; set; }
-        public bool Disclaimer { get; set; }
-        public List<string> MainVolList { get; set; }
-        public string MainVolSelected { get; set; }
+        public List<string> GamePayTable { get; set; }
+        public List<string> MaxBetLevel { get; set; }
+        public List<string> MaxCards { get; set; }
+        public List<string> CallSpeedMin { get; set; }
+        public List<string> CallSpeedMax { get; set; }
+        public List<string> CallSpeed { get; set; }
+        public List<string> CallSpeedBonus { get; set; }
+        public string GamePlayTableSelected { get; set; }
+        public string MaxBetLevelSelected { get; set; }
+        public string MaxCardsSelected { get; set; }
+        public string CallSpeedMinSelected { get; set; }
+        public string CallSpeedMaxSelected { get; set; }
+        public string CallSpeedSelected { get; set; }
+        public string CallSpeedBonusSelected { get; set; }
+        public bool AutoPlay { get; set; }
+        public bool HideSerialNumber { get; set; }
+        public bool SingleOfferBonus { get; set; }
     }
 }
