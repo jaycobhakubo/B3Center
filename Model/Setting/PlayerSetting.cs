b7c1d635a004361e7ab173c0eaff0da3f0f3653e
kkkkk <karlocamacho@gmail.com>
2017-01-22 02:51:22 -0800

    PlayerSetting.cs done

---
+++ Model/Setting/PlayerSetting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTech.Elite.Client.Modules.B3Center.Model.Setting
+{
+    public class PlayerSettings
+    {
+        public string TimeToCollect { get; set; }
+        public bool ScreenCursor { get; set; }
+        public bool CalibrateTouch { get; set; }
+        public bool AnnounceCall { get; set; }
+        public bool PressToCollect { get; set; }
+        public bool Disclaimer { get; set; }
+        public List<string> MainVolList { get; set; }
+        public string MainVolSelected { get; set; }
+    }
+}
