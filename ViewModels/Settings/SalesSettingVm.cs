ad44b7c80c4bf82f675e41a367362f2a31f67235
kkkkk <karlocamacho@gmail.com>
2017-01-23 04:58:31 -0800

    zcz`

---
+++ ViewModels/Settings/SalesSettingVm.cs
@@ -0,0 +1,36 @@
+﻿using GameTech.Elite.Base;
+using GameTech.Elite.Client.Modules.B3Center.Model.Setting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTech.Elite.Client.Modules.B3Center.ViewModels.Settings
+{
+    public class SalesSettingVm : ViewModelBase
+    {
+        private SalesSetting m_serverSetting;
+
+        public SalesSettingVm(ServerSetting _serversetting)
+        {
+            ServerSetting_ = _serversetting;
+        }
+
+        public ServerSetting ServerSetting_
+        {
+            get
+            {
+                return m_serverSetting;
+            }
+            set
+            {
+                m_serverSetting = value;
+                RaisePropertyChanged("ServerSetting_");
+            }
+        }
+    }
+}
+
+    {
+    }
+}
