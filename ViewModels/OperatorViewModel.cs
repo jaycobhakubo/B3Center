f9999cd917355811f13c99c3b5e6cbdd083d59cf
kkkkk <karlocamacho@gmail.com>
2017-01-25 23:58:05 -0800

    color ok on aperatpr

--- ViewModels/OperatorViewModel.cs
+++ ViewModels/OperatorViewModel.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Text;
 using GameTech.Elite.Client.Modules.B3Center.Business;
 using System.Windows.Controls;
-//using GameTech.Elite.Base;
 
 namespace GameTech.Elite.Client.Modules.B3Center.ViewModels
 {
@@ -14,19 +13,12 @@ namespace GameTech.Elite.Client.Modules.B3Center.ViewModels
     {
         public OperatorViewModel(ObservableCollection<Operator> operators_, List<B3IconColor> b3Iconcolor)
         {
-            OperatorColorList = b3Iconcolor;
+            B3IconColor = b3Iconcolor;
             Operators = operators_;
             SelectedOperator = Operators.FirstOrDefault();
            
         }
 
-
-        //private B3IconColor getIconColor(int colorId)
-        //{
-        //    B3IconColor operatorColor;
-
-        //        return;
-        //}
 
         private List<B3IconColor> m_B3IconColor;
         public List<B3IconColor> B3IconColor
@@ -40,7 +32,7 @@ namespace GameTech.Elite.Client.Modules.B3Center.ViewModels
         }
 
 
-
+      
 
         private B3IconColor m_selectedColor;
         public B3IconColor SelectedColor
@@ -63,18 +55,10 @@ namespace GameTech.Elite.Client.Modules.B3Center.ViewModels
             }
         }
 
-        //private ObservableCollection<Operator> m_operators;
         public ObservableCollection<Operator> Operators
         {
             get;set;
-            //get { return m_operators; }
-            //set
-            //{
-            //    m_operators = value;
-            //    RaisePropertyChanged("operators");
-            //}
         }
-
 
         private Operator m_selectedOperator;
         public Operator SelectedOperator
@@ -83,32 +67,10 @@ namespace GameTech.Elite.Client.Modules.B3Center.ViewModels
             set
             {
                 m_selectedOperator = value;
+                SelectedColor = m_B3IconColor.Single(l => l.ColorID == value.IconColor);
                 RaisePropertyChanged("selectedOperator");
             }
         }   
     }
 }
 
-
-//public Operator Operatorc
-//{
-//    get { return m_operator; }
-//    set
-//    {
-//        m_operator = value;
-//        SelectedColor = m_B3IconColor.Single(l => l.ColorID == value.IconColor);
-//        RaisePropertyChanged("Operatorc");
-
-//    }
-//}
-
-//private B3IconColor m_selectedColor;
-//public B3IconColor SelectedColor
-//{
-//    get { return m_selectedColor; }
-//    set
-//    {
-//        m_selectedColor = value;
-//        RaisePropertyChanged("SelectedColor");
-//    }
-//}
