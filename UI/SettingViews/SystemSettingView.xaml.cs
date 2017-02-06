2c81218f2b955c9a37cd71e95fb1b6d7c303e90d
kkkkk <karlocamacho@gmail.com>
2017-02-05 23:14:24 -0800

    fix issue on checkbox not allign

--- UI/SettingViews/SystemSettingView.xaml.cs
+++ UI/SettingViews/SystemSettingView.xaml.cs
@@ -15,26 +15,25 @@ namespace GameTech.Elite.Client.Modules.B3Center.UI.SettingViews
         public SystemSettingView(SystemSettingVm _systemSetting)
         {
             InitializeComponent();
-            FixLabelOrTextBlockContent();
             DataContext = _systemSetting;
 
         }
 
         //Im too lazy too type repeatable words. So I am going to do this.
         //Run once and never again.
-        private void FixLabelOrTextBlockContent()
-        {
-            foreach (UIElement element in gridSystemSettings.Children)
-            {
-                if (element is Label)
-                {
-                    var x = (Label)element;
-                    x.Content = "*" + x.Content + ":";
-                }
+        //private void FixLabelOrTextBlockContent()
+        //{
+        //    foreach (UIElement element in gridSystemSettings.Children)
+        //    {
+        //        if (element is Label)
+        //        {
+        //            var x = (Label)element;
+        //            x.Content = "*" + x.Content + ":";
+        //        }
 
 
 
-            }
-        }
+        //    }
+        //}
     }
 }
