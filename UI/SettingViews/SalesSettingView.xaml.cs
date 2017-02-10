ba81bb40a66b99471cf40952ed770c1fbed11f27
karlo <karlocamacho@gmail.com>
2017-02-10 12:51:45 -0800

    Sales user validation ok

--- UI/SettingViews/SalesSettingView.xaml.cs
+++ UI/SettingViews/SalesSettingView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+using GameTech.Elite.Client.Modules.B3Center.ViewModels;
 using GameTech.Elite.Client.Modules.B3Center.ViewModels.Settings;
 
 namespace GameTech.Elite.Client.Modules.B3Center.UI.SettingViews
@@ -14,5 +17,57 @@ namespace GameTech.Elite.Client.Modules.B3Center.UI.SettingViews
             InitializeComponent();
             DataContext = SessionSettingVm;
         }
+
+        private void ValidateUserInput(object sender, TextChangedEventArgs e)
+        {
+            TextBox currentTextBox = (TextBox)sender;
+
+            if (string.IsNullOrEmpty(currentTextBox.Text))
+            {
+                var ii = SettingViewModel.Instance;
+                ii.ViewReportVisibility = false;
+            }
+            else//If its not empty then lets validate all values.
+            {
+                var tempResult = false;
+                int tempResultInt;
+
+                if (int.TryParse(txtbxLogRecycleDays.Text.ToString(), out tempResultInt))
+                {                
+                    tempResult = true;
+                    var ii = SettingViewModel.Instance;
+                    ii.ViewReportVisibility = true;                                                                 
+                }
+
+                if (tempResult == false)
+                {
+                    var ii = SettingViewModel.Instance;
+                    ii.ViewReportVisibility = false;
+                }
+            }
+        }
+
+        private void DontAllowThisKeyboardinput(object sender, KeyEventArgs e)
+        {
+            bool notAllow = false;
+
+            if (e.Key == Key.Space)
+            {
+                notAllow = true;
+            }
+            else
+                if (e.Key == Key.Back)
+                {
+                    notAllow = false;
+                }
+            e.Handled = notAllow;
+        }
+
+
+        private void _PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }       
     }
 }
