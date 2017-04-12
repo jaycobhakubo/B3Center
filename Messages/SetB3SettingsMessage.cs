53d14dcda389af8956a2d83696de5b95729351c9
karlo <karlocamacho@gmail.com>
2017-04-12 12:13:59 -0700

    No overwrite saving.

--- Messages/SetB3SettingsMessage.cs
+++ Messages/SetB3SettingsMessage.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Collections.ObjectModel;
 using GameTech.Elite.Client.Modules.B3Center.Business;
 
@@ -11,19 +12,50 @@ namespace GameTech.Elite.Client.Modules.B3Center.Messages
 
         public SetB3SettingsMessage(ObservableCollection<B3SettingGlobal> lB3Settings)
         {
-            foreach (B3SettingGlobal sm in lB3Settings)
-            {
-                if (sm.B3SettingValue != sm.B3SettingdefaultValue)
-                {
-                    var settingMember = new SettingMember
-                    {
-                        GameType = sm.GameType,
-                        SettingType = sm.SettingType,
-                        Value = sm.B3SettingValue,
-                        OldValue = sm.B3SettingdefaultValue
-                    };
 
-                    m_lB3Settings.Add(settingMember);
+            var MathPayTableSetting = lB3Settings.Single(l => l.SettingType == B3SettingType.MathPayTableSetting);
+            if (MathPayTableSetting.B3SettingValue != MathPayTableSetting.B3SettingdefaultValue) //Check if it is a paytable setting  request.
+            {
+                var settingMember = new SettingMember()
+                {
+                    GameType = MathPayTableSetting.GameType,
+                    SettingType = MathPayTableSetting.SettingType,
+                    Value = MathPayTableSetting.B3SettingValue,
+                    OldValue = MathPayTableSetting.B3SettingdefaultValue
+                };
+
+                m_lB3Settings.Add(settingMember);//Add it on the very first list (it will excute first on the SP).
+
+                foreach (B3SettingGlobal sm in lB3Settings)//Add all settings. This will revert back to the current settings and not the default setting from paytable.
+                {
+                    if (sm.SettingType == B3SettingType.MathPayTableSetting) continue;
+                       settingMember = new SettingMember
+                        {
+                            GameType = sm.GameType,
+                            SettingType = sm.SettingType,
+                            Value = sm.B3SettingValue,
+                            OldValue = sm.B3SettingdefaultValue
+                        };
+
+                        m_lB3Settings.Add(settingMember);                
+                }
+            }
+            else 
+            {
+                foreach (B3SettingGlobal sm in lB3Settings)
+                {
+                    if (sm.B3SettingValue != sm.B3SettingdefaultValue)//filter only setting that need to be changed.
+                    {
+                        var settingMember = new SettingMember
+                        {
+                            GameType = sm.GameType,
+                            SettingType = sm.SettingType,
+                            Value = sm.B3SettingValue,
+                            OldValue = sm.B3SettingdefaultValue
+                        };
+
+                        m_lB3Settings.Add(settingMember);
+                    }
                 }
             }
         }
