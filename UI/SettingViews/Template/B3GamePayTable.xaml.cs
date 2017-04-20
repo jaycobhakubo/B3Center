e14820c1e5f2f2431394d31a2711da5d10b1ad12
karlo <karlocamacho@gmail.com>
2017-04-20 11:54:44 -0700

    Pay Table Settings UI completed.

---
+++ UI/SettingViews/Template/B3GamePayTable.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameTech.Elite.Client.Modules.B3Center.UI.SettingViews.Template
+{
+    /// <summary>
+    /// Interaction logic for B3GamePayTable.xaml
+    /// </summary>
+    public partial class B3GamePayTable : UserControl
+    {
+        public B3GamePayTable()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ViewModels/Settings/PayTableSettingVm.cs
+++ ViewModels/Settings/PayTableSettingVm.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace GameTech.Elite.Client.Modules.B3Center.ViewModels.Settings
 {
@@ -10,7 +11,16 @@ namespace GameTech.Elite.Client.Modules.B3Center.ViewModels.Settings
     {
         public PayTableSettingVm()
         {
-
+            var grdlength = new GridLength(1, GridUnitType.Star);
+            grdColumnB3GameName = grdlength;
+            RaisePropertyChanged("grdColumnB3GameName");
+            grdlength = new GridLength(3, GridUnitType.Star);
+            grdColumnPayTableSetting = grdlength;
+            RaisePropertyChanged("grdColumnPayTableSetting");
         }
+
+
+        public GridLength grdColumnB3GameName { get; set; }
+        public GridLength grdColumnPayTableSetting { get; set; }
     }
 }
